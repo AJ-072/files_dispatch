ce2914311e9a44f8d8dec17780d6a4115ed2e129
AH72 <abilashkj072@gmail.com>
2020-09-06 13:39:24 +0530

    trying Mvvm on this project

--- app/src/main/java/com/aj/filesdispatch/Models/FileViewItem.java
+++ app/src/main/java/com/aj/filesdispatch/Models/FileViewItem.java
@@ -6,7 +6,6 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.provider.MediaStore;
 
-import com.aj.filesdispatch.DataSource.AppListProvider;
 import com.aj.filesdispatch.common.Converter;
 
 import java.io.File;
@@ -21,7 +20,7 @@ public class FileViewItem extends FileData implements Parcelable {
     private Drawable fileDrawable;
     private long id;
 
-    public FileViewItem(Cursor cursor,String type) {
+    public FileViewItem(Cursor cursor, String type) {
         super(new File(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA))));
         this.fileDes = Converter.getFileDes(new File(cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA))));
         this.fileLoc = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA));
@@ -35,7 +34,8 @@ public class FileViewItem extends FileData implements Parcelable {
         this.fileLoc = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA));
         this.id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns._ID));
     }
-    public FileViewItem(String type,Cursor cursor,Drawable icon) {
+
+    public FileViewItem(String type, Cursor cursor, Drawable icon) {
         super(new File(cursor.getString(cursor.getColumnIndexOrThrow(FILE_LOC))));
         this.fileDes = Converter.getFileDes(new File(cursor.getString(cursor.getColumnIndexOrThrow(FILE_LOC))));
         this.fileLoc = cursor.getString(cursor.getColumnIndexOrThrow(FILE_LOC));
