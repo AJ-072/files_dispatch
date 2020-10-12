fa86efac1865d3f6f1d4ca077ac391f8c3e52700
AH72 <abilashkj072@gmail.com>
2020-10-13 00:41:02 +0530

    trying Mvvm on this project

--- app/src/main/java/com/aj/filesdispatch/Repository/AppListRepository.java
+++ app/src/main/java/com/aj/filesdispatch/Repository/AppListRepository.java
@@ -10,6 +10,7 @@ import android.content.pm.ResolveInfo;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LiveData;
 
 import com.aj.filesdispatch.DatabaseHelper.FileItemDatabase;
@@ -19,6 +20,7 @@ import com.aj.filesdispatch.Interface.FileItemDao;
 import com.aj.filesdispatch.common.Converter;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -28,7 +30,7 @@ public class AppListRepository {
     private LiveData<List<FileItem>> listLiveData;
     public static final String APPLICATION = "Applications";
     private static final String TAG = "FileItemRepository";
-    private static Context context;
+    private Context context;
     private UpdateList updateList;
 
     public AppListRepository(Application application) {
@@ -44,7 +46,7 @@ public class AppListRepository {
 
     public synchronized void UpdateList(List<FileItem> items) {
         if (updateList == null) {
-            updateList = new UpdateList(fileItemDao, items);
+            updateList = new UpdateList(fileItemDao, items,context);
             updateList.execute();
         }
     }
@@ -53,10 +55,11 @@ public class AppListRepository {
     static class UpdateList extends AsyncTask<Void, Void, Void> {
         private FileItemDao fileItemDao;
         private List<FileItem> fileItems;
-
-        UpdateList(FileItemDao fileItemDao, List<FileItem> fileItems) {
+        private WeakReference<Context> contextWeakReference;
+        UpdateList(FileItemDao fileItemDao, List<FileItem> fileItems, Context context) {
             this.fileItemDao = fileItemDao;
             this.fileItems = fileItems;
+            contextWeakReference= new WeakReference<>(context);
         }
 
         @Override
@@ -65,20 +68,20 @@ public class AppListRepository {
             Intent intent = new Intent(Intent.ACTION_MAIN, null);
             intent.addCategory(Intent.CATEGORY_LAUNCHER);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-            List<ResolveInfo> resolveInfo = context.getPackageManager().queryIntentActivities(intent, 0);
+            List<ResolveInfo> resolveInfo = contextWeakReference.get().getPackageManager().queryIntentActivities(intent, 0);
             if (fileItems == null || fileItems.size() == 0) {
                 for (ResolveInfo resolve : resolveInfo) {
                     ActivityInfo activityInfo = resolve.activityInfo;
                     if ((resolve.activityInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
                         packageName = activityInfo.packageName;
-                        File file = new File(Objects.requireNonNull(getSourceDir(packageName)));
+                        File file = new File(Objects.requireNonNull(getSourceDir(packageName,contextWeakReference.get())));
                         fileItemDao.insertFileItem(
                                 new FileItemBuilder(packageName)
-                                        .setFileName(getAppName(packageName))
+                                        .setFileName(getAppName(packageName,contextWeakReference.get()))
                                         .setFileSize(file.length())
                                         .setDateAdded(file.lastModified())
                                         .setFileType(APPLICATION)
-                                        .setFileUri(getSourceDir(packageName))
+                                        .setFileUri(getSourceDir(packageName,contextWeakReference.get()))
                                         .setShowDes(Converter.SizeInGMK(file.length()))
                                         .build());
                     }
@@ -95,15 +98,15 @@ public class AppListRepository {
                 for (FileItem item : fileItems) {
                     packageName = item.getFileId();
                     if (packageNames.contains(packageName)) {
-                        File file = new File(Objects.requireNonNull(getSourceDir(packageName)));
+                        File file = new File(Objects.requireNonNull(getSourceDir(packageName,contextWeakReference.get())));
                         if (item.getDateAdded() != file.lastModified()) {
                             fileItemDao.updateFileItem(
                                     new FileItemBuilder(packageName)
-                                            .setFileName(getAppName(packageName))
+                                            .setFileName(getAppName(packageName,contextWeakReference.get()))
                                             .setFileSize(file.length())
                                             .setDateAdded(file.lastModified())
                                             .setFileType(APPLICATION)
-                                            .setFileUri(getSourceDir(packageName))
+                                            .setFileUri(getSourceDir(packageName,contextWeakReference.get()))
                                             .setShowDes(Converter.SizeInGMK(file.length()))
                                             .build());
 
@@ -116,14 +119,14 @@ public class AppListRepository {
                 Log.d(TAG, "doInBackground: " + packageNames.size());
                 if (packageNames.size() > 0) {
                     for (String pack : packageNames) {
-                        File file = new File(Objects.requireNonNull(getSourceDir(pack)));
+                        File file = new File(Objects.requireNonNull(getSourceDir(pack,contextWeakReference.get())));
                         fileItemDao.insertFileItem(
                                 new FileItemBuilder(pack)
-                                .setFileName(getAppName(pack))
+                                .setFileName(getAppName(pack,contextWeakReference.get()))
                                 .setFileSize(file.length())
                                 .setDateAdded(file.lastModified())
                                 .setFileType(APPLICATION)
-                                .setFileUri(getSourceDir(pack))
+                                .setFileUri(getSourceDir(pack,contextWeakReference.get()))
                                 .setShowDes(Converter.SizeInGMK(file.length()))
                                 .build());
                     }
@@ -133,7 +136,7 @@ public class AppListRepository {
         }
     }
 
-    private static String getAppName(String pack_name) {
+    private static String getAppName(String pack_name,Context context) {
         try {
             return (String) context.getPackageManager().getApplicationLabel(context.getPackageManager().getApplicationInfo(pack_name, 0));
         } catch (PackageManager.NameNotFoundException e) {
@@ -142,7 +145,7 @@ public class AppListRepository {
         return null;
     }
 
-    private static String getSourceDir(String pack_name) {
+    private static String getSourceDir(String pack_name,Context context) {
         try {
             return context.getPackageManager().getApplicationInfo(pack_name, 0).sourceDir;
         } catch (PackageManager.NameNotFoundException e) {
